d9026d0c57aec50aedae6e7341330a65ca8c0351
vijay jadhav <64652691+vijay711-github@users.noreply.github.com>
2023-07-08 21:53:40 +0530

    Added the new fire-base Real time database

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://shopping-list-6af8d-default-rtdb.firebaseio.com/"
+
 }
 
 const app = initializeApp(appSettings)
